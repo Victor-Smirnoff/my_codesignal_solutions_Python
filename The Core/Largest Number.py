aba48d2b7ac771b0301b9439589f443da77bd350
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-03-15 17:16:25 +0300

    Create Largest Number.py

---
+++ The Core/Largest Number.py
@@ -0,0 +1,9 @@
+# https://app.codesignal.com/arcade/code-arcade/intro-gates/SZB5XypsMokGusDhX
+# Largest Number
+# Given an integer n, return the largest number that contains exactly n digits.
+# Example
+# For n = 2, the output should be
+# solution(n) = 99.
+
+def solution(n):
+    return int(n * '9')

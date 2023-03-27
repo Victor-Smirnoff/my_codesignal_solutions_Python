abd873c6ec19bf32720f6bc4410083c7628491bb
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-03-27 20:15:08 +0300

    Create Second-Rightmost Zero Bit.py

---
+++ The Core/Second-Rightmost Zero Bit.py
@@ -0,0 +1,13 @@
+# https://app.codesignal.com/arcade/code-arcade/corner-of-0s-and-1s/9nSj6DgqLDsBePJha
+# Second-Rightmost Zero Bit
+# Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+# Presented with the integer n, find the 0-based position of the second rightmost zero bit in its binary representation 
+# (it is guaranteed that such a bit exists), counting from right to left.
+# Return the value of 2position_of_the_found_bit.
+# Example
+# For n = 37, the output should be solution(n) = 8.
+# 3710 = 1001012. The second rightmost zero bit is at position 3 (0-based) from the right in the binary representation of n.
+# Thus, the answer is 23 = 8.
+
+def solution(n):
+    return 2 ** (len(str(bin(n)[2:])) - str(bin(n)[2:])[:str(bin(n)[2:]).rfind('0')].rfind('0') - 1)

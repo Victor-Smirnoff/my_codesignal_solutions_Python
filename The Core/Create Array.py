5e082a454299e38af03a7ca01e30d9f8651315c5
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-04-05 21:34:04 +0300

    Create Create Array.py

---
+++ The Core/Create Array.py
@@ -0,0 +1,8 @@
+# https://app.codesignal.com/arcade/code-arcade/list-forest-edge/gmZFbpR9cirL3Jpf2/solutions
+# Create Array
+# Given an integer size, return array of length size filled with 1s.
+# Example
+# For size = 4, the output should be solution(size) = [1, 1, 1, 1].
+
+def solution(size):
+    return [1] * size

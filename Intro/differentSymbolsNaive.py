436dd8ce4d2156fddff77bf25ef95608b377b9fa
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-05-08 11:31:51 +0300

    Create differentSymbolsNaive.py

---
+++ Intro/differentSymbolsNaive.py
@@ -0,0 +1,9 @@
+# differentSymbolsNaive
+# https://app.codesignal.com/arcade/intro/level-8/8N7p3MqzGQg5vFJfZ
+# Given a string, find the number of different characters in it.
+# Example
+# For s = "cabca", the output should be solution(s) = 3.
+# There are 3 different characters a, b and c.
+
+def solution(s):
+    return len(set(s))
